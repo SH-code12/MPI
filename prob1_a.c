cceeed533c85bf532964154ba4581ff1f767752a
Menna Reda <122902376+1Menna@users.noreply.github.com>
2025-05-05 16:37:22 +0300

    Update prob1_a.c

--- prob1_a.c
+++ prob1_a.c
@@ -1,4 +1,4 @@
-#include <mpi.h>
+include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -34,33 +34,36 @@ int main(int argc, char* argv[]) {
         printf("Please enter Upper bound...\n");
         scanf("%d", &y);
 
-        range = y - x;
+        range = (y - x) + 1;
         elements_per_process = range / (size - 1);
     }
 
     // Broadcast the essential variables to all processes
-    int bcast_data[3] = {range, elements_per_process, size};
-    MPI_Bcast(bcast_data, 3, MPI_INT, 0, MPI_COMM_WORLD);
+    int bcast_data[4] = {range, elements_per_process, size, x};
+    MPI_Bcast(bcast_data, 4, MPI_INT, 0, MPI_COMM_WORLD);
     range = bcast_data[0];
     elements_per_process = bcast_data[1];
     size = bcast_data[2]; // In case some MPI implementations modify size
-
+    x = bcast_data[3];
     //================================================
-    int start_index = (rank-1) * elements_per_process;
-    int end_index;
-    if(rank == size - 1){
-         end_index = start_index + elements_per_process;
+    int start_index=0 ,end_index=0;
+    if(rank != 0)
+    {
+       start_index = x +((rank-1) * elements_per_process);
+       end_index;
+       if(rank == size - 1){
+            end_index = start_index + (elements_per_process + ( range % (size - 1) ));}
+      else{
+          end_index = start_index + elements_per_process;
+          }
     }
-    else{
-         end_index = (start_index + elements_per_process) + ( range % (size - 1) );
-    }
+
     int subarray_count=0;
-    for(int i = start_index+1; i <= end_index; i++){
+    for(int i = start_index; i < end_index; i++){
           if(isPrime(i)){
              subarray_count++;
           }
     }
-
     // Reduce all local counts to get the total count at root
     int total_count;
     MPI_Reduce(&subarray_count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
